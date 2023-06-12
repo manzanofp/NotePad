f837b3ed5d1d28077c9999f900c48283aac305f2
manzanofp <felipemanzanofp@gmail.com>
2023-06-12 13:59:24 -0300

    criando as funções dos botões

--- notepad/src/main/java/felipemanzano/notepad/Notepad.java
+++ notepad/src/main/java/felipemanzano/notepad/Notepad.java
@@ -14,12 +14,24 @@ public class Notepad extends JFrame{
     JMenu editar = new JMenu("Editar");
     JMenu ajuda = new JMenu("Ajuda");
     
+    JMenuItem novoArquivo = new JMenuItem("Novo");
+    JMenuItem abrirArquivo = new JMenuItem("Abrir");
+    JMenuItem salvarArquivo = new JMenuItem("Salvar");
+    JMenuItem sair = new JMenuItem("sair");
+    
+    JMenuItem cortar = new JMenuItem("Cortar");
+    JMenuItem copiar = new JMenuItem("Copiar");
+    JMenuItem colar = new JMenuItem("Colar");
+    JMenuItem selecionarTudo = new JMenuItem("Selecionar Tudo");
+    
+    JMenuItem sobre = new JMenuItem("Sobre");
+    
     Notepad(){
         setTitle("Bloco de Notas");
         setBounds(100,100,800,600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-       add(barraMenu);
+       setJMenuBar(barraMenu);
        barraMenu.add(arquivo);
        barraMenu.add(editar);
        barraMenu.add(ajuda);
