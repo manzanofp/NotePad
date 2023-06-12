29aece9fe9a4c51f622646294f9f0be99d30f702
manzanofp <felipemanzanofp@gmail.com>
2023-06-12 12:26:35 -0300

    adicionando menus a tela

--- notepad/src/main/java/felipemanzano/notepad/Notepad.java
+++ notepad/src/main/java/felipemanzano/notepad/Notepad.java
@@ -10,13 +10,19 @@ public class Notepad extends JFrame{
 
     
     JMenuBar barraMenu = new JMenuBar();
+    JMenu arquivo = new JMenu("Arquivo");
+    JMenu editar = new JMenu("Editar");
+    JMenu ajuda = new JMenu("Ajuda");
     
     Notepad(){
         setTitle("Bloco de Notas");
         setBounds(100,100,800,600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-       
+       add(barraMenu);
+       barraMenu.add(arquivo);
+       barraMenu.add(editar);
+       barraMenu.add(ajuda);
     }
     
     public static void main(String[] args) {
